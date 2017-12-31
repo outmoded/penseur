397b255662297c965e0c0973aeac833480c4bf3b
Eran Hammer <eran@hammer.io>
2017-12-31 10:05:40 -0800

    Add each method to batch operations. Closes #148

--- test/table.js
+++ test/table.js
@@ -485,6 +485,41 @@ describe('Table', () => {
             expect(items.length).to.equal(100);
         });
 
+        it('inserts multiple records (each)', async () => {
+
+            const db = new Penseur.Db('penseurtest');
+            await db.establish(['test']);
+
+            const batches = [];
+            const orig = db.test._insert;
+            db.test._insert = (items, ...args) => {
+
+                batches.push(Array.isArray(items) ? items.length : 'single');
+                return orig.call(db.test, items, ...args);
+            };
+
+            const records = [];
+            const ids = [];
+            for (let i = 1; i < 101; ++i) {
+                records.push({ id: i, a: i });
+                ids.push(i);
+            }
+
+            const updates = [];
+            const keys = await db.test.insert(records, { chunks: 30, each: (s, is) => updates.push({ i: s, ids: is }) });
+            expect(keys).to.equal(ids);
+            expect(batches).to.equal([30, 30, 30, 10]);
+            expect(updates).to.equal([
+                { i: 0, ids: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30] },
+                { i: 1, ids: [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60] },
+                { i: 2, ids: [61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90] },
+                { i: 3, ids: [91, 92, 93, 94, 95, 96, 97, 98, 99, 100] }
+            ]);
+
+            const items = await db.test.all();
+            expect(items.length).to.equal(100);
+        });
+
         it('inserts a record (ignore batch on non array)', async () => {
 
             const db = new Penseur.Db('penseurtest');
@@ -1041,6 +1076,40 @@ describe('Table', () => {
 
             await db.test.update(updates, { chunks: 30 });
             expect(batches).to.equal([30, 30, 30, 10]);
+
+            const item = await db.test.get(1);
+            expect(item.a).to.not.exist();
+        });
+
+        it('updates multiple records (chunks + each)', async () => {
+
+            const db = new Penseur.Db('penseurtest');
+            await db.establish(['test']);
+
+            const records = [];
+            for (let i = 1; i < 101; ++i) {
+                records.push({ id: i, a: i });
+            }
+
+            await db.test.insert(records);
+
+            const batches = [];
+            const orig = db.test._update;
+            db.test._update = (ids, ...args) => {
+
+                batches.push(ids.length);
+                return orig.call(db.test, ids, ...args);
+            };
+
+            const updates = [];
+            for (let i = 1; i < 101; ++i) {
+                updates.push({ id: i, a: db.unset() });
+            }
+
+            const reports = [];
+            await db.test.update(updates, { chunks: 30, each: (s) => reports.push(s) });
+            expect(batches).to.equal([30, 30, 30, 10]);
+            expect(reports).to.equal([0, 1, 2, 3]);
 
             const item = await db.test.get(1);
             expect(item.a).to.not.exist();
